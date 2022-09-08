247c02747fb5847296a6ff25b34b6eeefa8d900f
vishalsingh17 <vishal170997@gmail.com>
2022-09-08 12:33:28 +0530

    updated main.py

--- main.py
+++ main.py
@@ -25,6 +25,21 @@ def create_blog(request: schemas.Blog, db:Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id:int, db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return f"Blog with id {id} deleted successfully!!"
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id:int, request: schemas.Blog, db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.update(request) 
+    db.commit()
+    return f"Blog with id {id} updated successfully!!"
+
 @app.get('/blog')
 def get_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
